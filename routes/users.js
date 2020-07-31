0b7907d990eb7cb8595d80ad0082f4ca8e9581ee
Connor Chaffin <cchaffin43@gmail.com>
2020-07-31 15:58:24 -0500

    fixed all errors in users.js

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.SERVER_PORT||8000;
+const port = process.env.SERVER_PORT || 8000;
 
 /* GET users listing. */
 app.use((req, res, next) => {
@@ -22,10 +22,6 @@ app.get("/api", (req, res) => {
   res.json("Hello");
 });
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-var bodyParser = require('body-parser');
-const app = express();
-const port = process.env.SERVER_PORT || 8000;
 
 
 /* GET users listing. */
@@ -49,7 +45,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
   res.render('signup');
 });*/
 
-app.post('/signup', function(req, res, next) {
+app.post('/signup', function (req, res, next) {
   models.users
     .findOrCreate({
       where: {
@@ -62,13 +58,9 @@ app.post('/signup', function(req, res, next) {
         Password: req.body.password
       }
     })
-    .spread(function(result, created) {
+    .spread(function (result, created) {
       if (created) {
-<<<<<<< HEAD
-        res.json("Hello"); 
-=======
-        res.json('index');
->>>>>>> 003304b47103b9a4de72533c0d1c41f25926d89b
+        res.json("Hello");
       } else {
         res.send('This user already exists');
       }
@@ -77,7 +69,6 @@ app.post('/signup', function(req, res, next) {
 
 
 //Login user and return JWT as cookie post below
-<<<<<<< HEAD
 
 
 router.post('/login', function (req, res, next) {
@@ -103,7 +94,7 @@ router.post('/login', function (req, res, next) {
     }
   });
 });
-=======
+
 /*router.get('/login', function(req, res, next) {
   res.render('login');
 <<<<<<< HEAD
@@ -137,7 +128,6 @@ router.post('/login', function (req, res, next) {
 //   }
 //   });
 // });
->>>>>>> 003304b47103b9a4de72533c0d1c41f25926d89b
 
 //need get method to pull profile page of user
 router.get('/profile', function (req, res, next) {
@@ -153,20 +143,18 @@ router.get('/profile', function (req, res, next) {
         }
       });
   } else {
-<<<<<<< HEAD
-    res.status(401);
-    res.send('Must be logged in');
-=======
     let status;
     if (req.user.Admin) {
       status = 'Admin';
-=======
-}); */
+    }
+  }
+});
+
 router.post('/login', function (req, res, next) {
   models.users.findOne({
     where: {
       Username: req.body.username,
-      
+
     }
   }).then(user => {
     if (!user) {
@@ -174,27 +162,26 @@ router.post('/login', function (req, res, next) {
       return res.status(401).json({
         message: "Login Failed"
       });
-    }else{    
-      let passwordMatch = authService.comparePasswords(req.body.password, user.Password); 
-      if(passwordMatch){
+    } else {
+      let passwordMatch = authService.comparePasswords(req.body.password, user.Password);
+      if (passwordMatch) {
         let token = authService.signUser(user);
         res.cookie('jwt', token); //this doesn't work res.json
         res.send('Login successful');
->>>>>>> 10f2ce2cb4e874896f7fd60ad2285f5592f5ef51
-    } else {
-      status = 'Normal user';
-    }
+      } else {
+        status = 'Normal user';
+      }
 
-    res.render('profile', {
-      FirstName: req.user.FirstName,
-      LastName: req.user.LastName,
-      Email: req.user.Email,
-      UserId: req.user.UserId,
-      Username: req.user.Username,
-      Status: status
-    });
->>>>>>> 003304b47103b9a4de72533c0d1c41f25926d89b
-  }
+      res.render('profile', {
+        FirstName: req.user.FirstName,
+        LastName: req.user.LastName,
+        Email: req.user.Email,
+        UserId: req.user.UserId,
+        Username: req.user.Username,
+        Status: status
+      });
+    }
+  });
 });
 
 
@@ -203,6 +190,6 @@ router.post('/login', function (req, res, next) {
 router.get('/logout', function (req, res, next) {
   res.cookie('jwt', "", { expires: new Date(0) });
   res.send('Logged out');
-  });
+});
 
 module.exports = router;

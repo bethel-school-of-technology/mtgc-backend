613a35a1524b13c2653be720b74cef815c412752
scottyg68 <59403591+scottyg68@users.noreply.github.com>
2020-08-17 13:59:06 -0600

    fixed errors

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var models = require('../models');
 var authService = require('../services/auth');
 const mysql = require('mysql2');
-
+const config = require("../config/auth.config");
 var bodyParser = require('body-parser');
 
 
@@ -22,73 +22,85 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/api", (req, res) => {
-  res.json("Hello");
-});
+
 
 
 
 //need post for creating a new user 
-/*router.get('/signup', function(req, res, next) {
-  res.render('signup');
-});*/
+
 
 app.post('/signup', function (req, res, next) {
+  // Save User to Database
   models.users
-    .findOrCreate({
-      where: {
-        Username: req.body.username
-      },
-      defaults: {
-        FirstName: req.body.firstName,
-        LastName: req.body.lastName,
-        Email: req.body.email,
-        Password: req.body.password
-      }
-    })
-    .spread(function (result, created) {
-      if (created) {
-        res.json("Hello");
-      } else {
-        res.send('This user already exists');
-      }
-    });
+  .findOrCreate({
+    where: {
+      Username: req.body.username
+    },
+    defaults: {   
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.username,email,
+    username: req.body.username, 
+    password: bcrypt.hashSync(req.body.password, 8),
+    missionary: req.body.missionary,
+    phoneNumber: req.body.phoneNumber,
+    bio: req.body.bio,
+  }
+})
+.spread(function (result, created) {
+  if (created) {
+    res.json("Hello");
+  } else {
+    res.send('This user already exists');
+  }
 });
+});
+
+router.post('/signin') = (req, res,next) => {
+  models.users
+  Users.findOne({
+    where: {
+      username: req.body.username
+    }
+  })
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({ message: "User Not found." });
+      }
+
+      var passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          accessToken: null,
+          message: "Invalid Password!"
+        });
+      }
+
+      var token = jwt.sign({ id: user.id }, config.secret, {
+        expiresIn: 86400 // 24 hours
+      });
+
+     
+    })
+  }
+
 
 router.get('/profile', function (req, res, next) {
   models.users
     .findAll({ })
-    .then(usersFound => {
+    .then(users => {
       res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify(usersFound));
+      res.json(users);
     })
 });
 //Login user and return JWT as cookie post below
 
 
-router.post('/login', function (req, res, next) {
-  models.users.findOne({
-    where: {
-      Username: req.body.username,
-      Password: req.body.password
-    }
-  }).then(user => {
-    if (!user) {
-      console.log('User not found')
-      return res.status(401).json({
-        message: "Login Failed"
-      });
-    }
-    if (user) {
-      let token = authService.signUser(user); // <--- Uses the authService to create jwt token
-      res.cookie('jwt', token); // <--- Adds token to response as a cookie
-      res.send(JSON.stringify('users'));
-    } else {
-      console.log('Wrong password');
-      res.redirect('login')
-    }
-  });
-});
+
 
 //need get mtehod to pull profile page of user
 /* router.get('/profile', function (req, res, next) {
@@ -108,86 +120,13 @@ router.post('/login', function (req, res, next) {
     });
 }); */
 
-// router.post('/login', function (req, res, next) {
-//   models.users.findOne({
-//     where: {
-//       Username: req.body.username
-//     }
-//   }).then(user => {
-//     if (!user) {
-//       console.log('User not found')
-//       return res.status(401).json({
-//         message: "Login Failed"
-//       });
-//     } else {
-//       let passwordMatch = authService.comparePasswords(req.body.password, user.Password);
-//       if (passwordMatch) {
-//         let token = authService.signUser(user);
-//         res.cookie('jwt', token);
-//         res.send('Login successful');
-//       } else {
-//         console.log('Wrong password');
-//         res.send('Wrong password');
-//       }
-//     }
-//   }
-//   });
-// });
 
-//need get method to pull profile page of user
-// router.get('/profile', function (req, res, next) {
-//   let token = req.cookies.jwt;
-//   if (token) {
-//     authService.verifyUser(token)
-//       .then(user => {
-//         if (user) {
-//           res.send(JSON.stringify(user));
-//         } else {
-//           res.status(401);
-//           res.send('Invalid authentication token');
-//         }
-//       });
-//   } else {
-//     let status;
-//     if (req.user.Admin) {
-//       status = 'Admin';
-//     }
-//   }
-// });
 
-// router.post('/login', function (req, res, next) {
-//   models.users.findOne({
-//     where: {
-//       Username: req.body.username,
 
-//     }
-//   }).then(user => {
-//     if (!user) {
-//       console.log('User not found')
-//       return res.status(401).json({
-//         message: "Login Failed"
-//       });
-//     } else {
-//       let passwordMatch = authService.comparePasswords(req.body.password, user.Password);
-//       if (passwordMatch) {
-//         let token = authService.signUser(user);
-//         res.cookie('jwt', token); //this doesn't work res.json
-//         res.send('Login successful');
-//       } else {
-//         status = 'Normal user';
-//       }
 
-//       res.render('profile', {
-//         FirstName: req.user.FirstName,
-//         LastName: req.user.LastName,
-//         Email: req.user.Email,
-//         UserId: req.user.UserId,
-//         Username: req.user.Username,
-//         Status: status
-//       });
-//     }
-//   });
-// });
+
+
+
 
 /* List all users for admin */
 // router.get('/admin', function (req, res, next) {
